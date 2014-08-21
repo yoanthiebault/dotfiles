0a90285334bffabd374bd9ac492111c5ffdff4bc
Thibaut Tiberghien <thibaut.tiberghien@ipal.cnrs.fr>
2014-08-21 16:16:02 +0000

    ELPA install of web-beautify instead of manual one.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -99,7 +99,16 @@
 (ac-config-default)
 
 ;; web-beautify for js, css, html
-(load "web-beautify.el")
+;;(require 'web-beautify) ;; Not necessary if using ELPA package
+(eval-after-load 'js2-mode
+  '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
+(eval-after-load 'json-mode
+  '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+(eval-after-load 'sgml-mode
+  '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+(eval-after-load 'css-mode
+  '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
+
 
 ;; flycheck mode permanently
 (add-hook 'after-init-hook #'global-flycheck-mode)
